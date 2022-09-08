45e17f4f16114de7fe78f2cd4a85df761504b423
UpekshaHerath <upekshadilshan000@gmail.com>
2022-09-08 07:25:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import loginImage from './images/loginImage.jpg'
+import { height } from '@mui/system';
+import { Button } from '@mui/material';
+import SearchAppBar from './components/SearchAppBar';
 
 function App() {
+
+  const loginFunctionality = () => {
+    const userName = document.getElementById('userName');
+    const password = document.getElementById('passWord');
+    
+    if (userName.value == 'Upeksha' && password.value == '1234') {
+      alert('login successfull !');
+    } else {
+      alert('login failed !');
+    }
+    userName.value = "";
+    password.value = "";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <SearchAppBar />
+      </div>
+      <div className='loginCardMain'>
+        <div className='loginCard'>
+            <img src={loginImage} alt="login image" className='image'/>
+
+            <div className='flexContainer'>
+              <div className='title'>
+                <h2>Fitlabs</h2>
+                <h3 style={{color: "blue"}}>login</h3>
+              </div>
+              <form action="" className='form'>
+                <label>User Name</label>
+                <input type="text" id='userName'/>
+                <label>Password</label>
+                <input type="text" id='passWord'/>
+                <Button variant="contained" onClick={loginFunctionality}>Login</Button>
+              </form>
+            </div>
+        </div>
+      </div>
+      
+    </>
+    
+    
   );
 }
 
